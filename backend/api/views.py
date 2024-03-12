343d1d2c12a41f0326ca04d751d287d26a03ba7c
asekalinin <asekalinin@greenatom.ru>
2024-03-12 21:23:59 +0300

    fix import UserSerializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions, viewsets
 
-from api.serializers import UserSerializer
+from root.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
