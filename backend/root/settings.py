c395492ddf120e90537c87d60f34af08babe4a4d
ASeKalinin <asekalinin@greenatom.ru>
2024-03-13 23:32:11 +0300

    fix CSRF token lost

--- backend/root/settings.py
+++ backend/root/settings.py
@@ -90,7 +90,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.SessionAuthentication", # TODO: включить для DRF
     ),
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
