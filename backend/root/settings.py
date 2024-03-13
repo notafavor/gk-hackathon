31e18dc1ef49f5a2c29b63d4afdaeaa0ac07ac08
ASeKalinin <asekalinin@greenatom.ru>
2024-03-13 18:21:09 +0300

    CORS headers

--- backend/root/settings.py
+++ backend/root/settings.py
@@ -40,12 +40,12 @@ INSTALLED_APPS = INSTALLED_APPS + APPS
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "root.urls"
