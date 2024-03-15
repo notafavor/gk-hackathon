9ce86a80de78db89962c8753a572daa7db9617d1
asekalinin <asekalinin@greenatom.ru>
2024-03-16 00:30:00 +0300

    Жизнь токена продлена

--- backend/root/settings.py
+++ backend/root/settings.py
@@ -104,7 +104,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=60),
 }
 
